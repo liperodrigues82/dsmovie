75a185c021a55cc8f5e465ee600a2a21e0e4a8e4
Felipe Rodrigues <68449279+liperodrigues82@users.noreply.github.com>
2022-03-08 16:04:06 -0300

    ReStart the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,25 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import Listing from 'pages/Listing';
-import Form from 'pages/Form';
-import Navbar from "components/Navbar";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Listing />} />
-        <Route path="/form">
-          <Route path=":movieId" element={<Form />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
